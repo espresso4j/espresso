a23d7ece6ca6b1e44057d6ccdd328e1b70b67062
Ning Sun <sunng@protonmail.com>
2018-10-06 21:54:00 +0800

    (chore) initial commit

---
+++ src/main/java/io/github/espresso4j/App.java
@@ -0,0 +1,13 @@
+package io.github.espresso4j;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
